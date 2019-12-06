1ab040e5c87b2516dcf36b918d1ee3da4bee9eb4
shengyajie <13733858073@163.com>
2019-12-06 20:16:21 +0800

    绑定按钮跳转，详情页navbar

--- pages/det/det.js
+++ pages/det/det.js
@@ -1,110 +1,27 @@
-var items=require("../../data/miaodetail.js")
-var app = getApp()
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
   data: {
-    // items: [
-    //   {
-    //     img: "/images/indimgs/spring.png",
-    //     text: "出行",
-    //     spent: "1000",
-    //     postid: 0
-    //   },
-    //   {
-    //     img: "/images/indimgs/boy.png",
-    //     text: "出行",
-    //     spent: "1000",
-    //     postid: 1
-    //   }, {
-    //     img: "/images/indimgs/girl.png",
-    //     text: "出行",
-    //     spent: "1000",
-    //     postid: 2
-    //   }, {
-    //     img: "/images/indimgs/baozi.png",
-    //     text: "出行",
-    //     spent: "1000",
-    //     postid: 3
-    //   }, {
-    //     img: "/images/indimgs/girl.png",
-    //     text: "出行",
-    //     spent: "1000",
-    //     postid: 4
-    //   }, {
-    //     img: "/images/indimgs/boy.png",
-    //     text: "出行",
-    //     spent: "1000",
-    //     postid: 5
-    //   }
-    // ],
-    
-    startX: 0, //开始坐标
-    startY: 0
+    tabs: ["支出", "收入"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
   },
   onLoad: function () {
-   
-      // this.data.items.push({
-      //   content:  " ",
-      //   isTouchMove: false //默认全隐藏删除
-      // })
-    this.setData({
-      miaode:items.postList
-    })
-  },
-  //手指触摸动作开始 记录起点X坐标
-  touchstart: function (e) {
-    //开始触摸时 重置所有删除
-    this.data.miaode.forEach(function (v, i) {
-      if (v.isTouchMove)//只操作为true的
-        v.isTouchMove = false;
-    })
-    this.setData({
-      startX: e.changedTouches[0].clientX,
-      startY: e.changedTouches[0].clientY,
-      miaode:items.postList
-    })
-  },
-  //滑动事件处理
-  touchmove: function (e) {
-    var that = this,
-      index = e.currentTarget.dataset.index,//当前索引
-      startX = that.data.startX,//开始X坐标
-      startY = that.data.startY,//开始Y坐标
-      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
-      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
-      //获取滑动角度
-      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
-    that.data.miaode.forEach(function (v, i) {
-      v.isTouchMove = false
-      //滑动超过30度角 return
-      if (Math.abs(angle) > 30) return;
-      if (i == index) {
-        if (touchMoveX > startX) //右滑
-          v.isTouchMove = false
-        else //左滑
-          v.isTouchMove = true
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
       }
-    })
-    //更新数据
-    that.setData({
-      miaode:items.postList
-    })
+    });
   },
-  /**
-   * 计算滑动角度
-   * @param {Object} start 起点坐标
-   * @param {Object} end 终点坐标
-   */
-  angle: function (start, end) {
-    var _X = end.X - start.X,
-      _Y = end.Y - start.Y
-    //返回角度 /Math.atan()返回数字的反正切值
-    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-  },
-  //删除事件
-  del: function (e) {
-    this.data.miaode.splice(e.currentTarget.dataset.index, 1)
+  tabClick: function (e) {
     this.setData({
-      miaode:items.postList
-    })
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   }
-})
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -103,42 +103,45 @@ Page({
   },
 
 //按钮移动
-  buttonStart: function(e) {
-    startPoint = e.touches[0]
-  },
-  buttonMove: function(e) {
-    var endPoint = e.touches[e.touches.length - 1]
-    var translateX = endPoint.clientX - startPoint.clientX
-    var translateY = endPoint.clientY - startPoint.clientY
-    startPoint = endPoint
-    var buttonTop = this.data.buttonTop + translateY
-    var buttonLeft = this.data.buttonLeft + translateX
-    //判断是移动否超出屏幕
-    if (buttonLeft + 50 >= this.data.windowWidth) {
-      buttonLeft = this.data.windowWidth - 50;
-    }
-    if (buttonLeft <= 0) {
-      buttonLeft = 0;
-    }
-    if (buttonTop <= 0) {
-      buttonTop = 0
-    }
-    if (buttonTop + 50 >= this.data.windowHeight) {
-      buttonTop = this.data.windowHeight - 50;
-    }
-    this.setData({
-      buttonTop: buttonTop,
-      buttonLeft: buttonLeft
-    })
-  },
+  // buttonStart: function(e) {
+  //   startPoint = e.touches[0]
+  // },
+  // buttonMove: function(e) {
+  //   var endPoint = e.touches[e.touches.length - 1]
+  //   var translateX = endPoint.clientX - startPoint.clientX
+  //   var translateY = endPoint.clientY - startPoint.clientY
+  //   startPoint = endPoint
+  //   var buttonTop = this.data.buttonTop + translateY
+  //   var buttonLeft = this.data.buttonLeft + translateX
+  //   //判断是移动否超出屏幕
+  //   if (buttonLeft + 50 >= this.data.windowWidth) {
+  //     buttonLeft = this.data.windowWidth - 50;
+  //   }
+  //   if (buttonLeft <= 0) {
+  //     buttonLeft = 0;
+  //   }
+  //   if (buttonTop <= 0) {
+  //     buttonTop = 0
+  //   }
+  //   if (buttonTop + 50 >= this.data.windowHeight) {
+  //     buttonTop = this.data.windowHeight - 50;
+  //   }
+  //   this.setData({
+  //     buttonTop: buttonTop,
+  //     buttonLeft: buttonLeft
+  //   })
+  // },
 
-  buttonEnd: function(e) {
+  // buttonEnd: function(e) {
 
-  },
+  // },
 // 点击按钮
-  clickadd:function(event){
+  adddetail: function () {
     wx.navigateTo({
-      url: 'pages/det/det',
+      url: '../det/det',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
     })
-  },
+  }
 })
