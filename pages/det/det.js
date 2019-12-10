7ce6de6a3b44c4d5234cbcfcc90be35272ecd0bd
shengyajie <13733858073@163.com>
2019-12-10 16:47:31 +0800

    分类选择择

--- pages/det/det.js
+++ pages/det/det.js
@@ -5,7 +5,60 @@ Page({
     tabs: ["支出", "收入"],
     activeIndex: 1,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    imgHoverIndex:0,
+    imgList:[
+      {
+        url:'../../images/shop/cashg0.png',
+        hoverUrl:'../../images/shop/cashg1.png',
+        title:'通讯',
+        tecolor: '#616161',
+        hocolor: "#000"
+      },
+      {
+        url:'../../images/shop/comm0.png',
+        hoverUrl:'../../images/shop/comm1.png',
+        title: '娱乐',
+        tecolor: '#616161',
+        hocolor: "#000"
+      },
+      {
+        url: '../../images/shop/comm0.png',
+        hoverUrl: '../../images/shop/comm1.png',
+        title: '住房',
+        tecolor: '#616161',
+        hocolor: "#000"
+      },
+      {
+        url: '../../images/shop/comm0.png',
+        hoverUrl: '../../images/shop/comm1.png',
+        title: '化妆品',
+        tecolor: '#616161',
+        hocolor: "#000"
+      },
+      {
+        url: '../../images/shop/comm0.png',
+        hoverUrl: '../../images/shop/comm1.png',
+        title: '医疗',
+        tecolor: '#616161',
+        hocolor: "#000"
+      },
+      {
+        url: '../../images/shop/comm0.png',
+        hoverUrl: '../../images/shop/comm1.png',
+        title: '宠物',
+        tecolor: '#616161',
+        hocolor:"#000"
+      }
+    ]
+  },
+  chooseThis(e) {
+    this.setData({
+      imgHoverIndex: e.currentTarget.dataset.index,
+    })
+  },
+  show:function(){
+   
   },
   onLoad: function () {
     var that = this;
